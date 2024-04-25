b769005880a259174bd0a1191f30b79d4a1d2b0d
OscarPereyra1992 <oscarpereyra.mhs@gmail.com>
2024-04-25 09:38:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Home from "./Home"
+import Navbar from "./Navbar"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.tsx
@@ -0,0 +1,16 @@
+import BoatVideo from "./BoatVideo";
+import HeroText from "./HeroText";
+import MapImages from "./MapImages";
+
+function Home (){
+    return(
+        <>
+        <HeroText />
+        <MapImages />
+        <BoatVideo/>
+
+        </>
+    )
+}
+
+export default Home;
